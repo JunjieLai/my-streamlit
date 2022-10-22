72401f7fed1bc37db0babcd82c26d2970d022b17
JunjieLai <2450883958@qq.com>
2022-10-23 01:09:28 +0800

    first vision

--- assignment-06-st-starter.py
+++ assignment-06-st-starter.py
@@ -19,5 +19,10 @@ fig, ax = plt.subplots(1, 3, figsize=(15,5))
 df[df.Pclass == 1].Fare.plot.box(ax=ax[0])  
 df[df.Pclass == 2].Fare.plot.box(ax=ax[1])  
 df[df.Pclass == 3].Fare.plot.box(ax=ax[2]) 
+ax[0].set_xlabel('Pclass = 1')
+ax[1].set_xlabel('Pclass = 2') 
+ax[2].set_xlabel('Pclass = 3') 
+
+ax[0].set_ylabel('Fare')
 
 st.pyplot(fig)
